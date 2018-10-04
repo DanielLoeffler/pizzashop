79247ace4b677afc15a833f37e096fd795fc723d
Daniel Löffeler <daniel.loeffler@ti8m.ch>
2018-10-04 15:57:59 +0200

    Fix Template (list)

--- src/main/java/ch/ti8m/azubi/lod/pizzashop/web/PizzaServlet.java
+++ src/main/java/ch/ti8m/azubi/lod/pizzashop/web/PizzaServlet.java
@@ -26,27 +26,6 @@ public class PizzaServlet extends HttpServlet {
         template = new FreemarkerConfig().loadTemplate("pizzashop.ftl");
     }
 
-/*
-    private Configuration configuration() {
-        Configuration config = new Configuration();
-        config.setDefaultEncoding("UTF-8");
-        config.setLocale(Locale.getDefault());
-        config.setClassForTemplateLoading(getClass(), "/templates");
-        config.setIncompatibleImprovements(new Version(2, 3, 20));
-        config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        return config;
-    }
-
-
-    public Template loadTemplate(String resourcePath) throws ServletException {
-        try {
-            return configuration().getTemplate(resourcePath);
-        } catch (IOException e) {
-            throw new ServletException("Failed to load Template" + e.getMessage());
-        }
-    }
-*/
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
@@ -138,92 +117,6 @@ public class PizzaServlet extends HttpServlet {
 
 */
 
-
-/*
-        writer.write("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "\t<meta charset=\"utf-8\">\n" +
-                "\t\t<title>\n" +
-                "\t\t\tPizzashop HTML\n" +
-                "\t\t</title>\n" +
-                "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"Pizza.css\">\n" +
-                "</head>\n" +
-                "<body background=\"http://cdn4.micasa.ch/medias/sys_master/zoom/1/c/5/6/id_10295631904798_zoom.jpg\"><b>\n" +
-                "<a href=\"Pizza Restaurant.html\">\n" +
-                "\t<h1>Pizzeria HTML</h1>\n" +
-                "</a><br>\n" +
-                "<br>\n" +
-                "Pizza lieferung:\n" +
-                "<a href=\"https://www.dieci.ch/de/\">\n" +
-                "bitte hier klicken um Die bestellung zu machen.<br>\n" +
-                "</a><br>\n" +
-                "Zur auswahl stehen folgende Sorten mit dem Preis f&uumlr die Kleine Pizza\n" +
-                "<br>\n" +
-                "<br>\n" +
-                "<button type=\"button\">show all</button>\n" +
-                "<br>\n" +
-                "<br>\n" +
-                "show Pizza <form><label for=\"idzahl\">Nummer: <input id=\"idzahl\" name=\"idzahl\"></label></form>" +
-                "<br>\n" +
-                "<br>\n" +
-                pizza +
-                "<br>\n" +
-                "<br>\n" +
-                "<form> creat<label for=\"idcreate\">Id: <input id=\"idcreate\" name=\"idcreate\"></label>" +
-                "<br>\n" +
-                "<br>\n" +
-                "creat <label for=\"namecreate\">Name: <input id=\"namecreate\" name=\"namecreate\"></label>" +
-                "<br>\n" +
-                "<br>\n" +
-                "creat <label for=\"pricecreate\">Price: <input id=\"pricecreate\" name=\"pricecreate\"></label></form>" +
-                "<br>\n" +
-                "<br>\n" +
-                createPizza +
-                "<br>\n" +
-                "<br>\n" +
-                "<table>\n" +
-                "\t<tr>\n" +
-                "\t\t<th>Name</th>\n" +
-                "\t\t<th>Preis</th>\n" +
-                "\t\t<th>Bild</th>\n" +
-                "\t</tr>\n" +
-                "\t<tr>\n" +
-                "\t\t<td>Pizza Margherita</td>\n" +
-                "\t\t<td>6.-</td>\n" +
-                "\t\t<td><img src=\"http://www.cbc.ca/inthekitchen/assets_c/2012/11/MargheritaPizza21-thumb-596x350-247022.jpg\" width=\"200\"></td>\n" +
-                "\t</tr>\n" +
-                "\t<tr>\n" +
-                "\t\t<td>Pizza Hawaii</td>\n" +
-                "\t\t<td>6.50 Fr.</td>\n" +
-                "\t\t<td><img src=\"https://images.kurier.at/46-90332057.jpg/620x340/248.134.343\"width=\"200\"></td>\n" +
-                "\t</tr>\n" +
-                "\t<tr>\n" +
-                "\t\t<td>Pizza Napoli</td>\n" +
-                "\t\t<td>7.50 Fr.</td>\n" +
-                "\t\t<td><img src=\"https://media-cdn.tripadvisor.com/media/photo-s/0d/11/36/0c/pizza-napoli.jpg\"width=\"200\"></td>\n" +
-                "\t</tr>\n" +
-                "\t<tr>\n" +
-                "\t\t<td>Pizza Verdi</td>\n" +
-                "\t\t<td>5.50 Fr.</td>\n" +
-                "\t\t<td><img src=\"https://media-cdn.tripadvisor.com/media/photo-s/0a/80/8a/21/pizza-verdi.jpg\"width=\"200\"></td>\n" +
-                "\t</tr>\n" +
-                "\t<tr>\n" +
-                "\t\t<td>&#9733 Pizza a la HTML &#9733</td>\n" +
-                "\t\t<td>8.-</td>\n" +
-                "\t\t<td><img src=\"http://blog.prospective.ch/wp-content/uploads/2012/08/qr-code.png\"width=\"200\"></td>\n" +
-                "\t</tr>\n" +
-                "</table><br>\n" +
-                "<br>\n" +
-                "\n" +
-                "\n" +
-                "</b>\n" +
-                "</body>\n" +
-                "</html>");
-
-*/
-
-
     }
 
     @Override
@@ -235,13 +128,6 @@ public class PizzaServlet extends HttpServlet {
         String namecreate = req.getParameter("namecreate");
         String pricecreate = req.getParameter("pricecreate");
 
-        /*
-        String button = req.getParameter("button");
-
-        if ("button".equals(button)) {
-            pizzaService.list();
-        }
-        */
 
         int idCreateInt = pizzaService.idToInt(idcreate);
         double priceCreateDouble = pizzaService.priceToDouble(pricecreate);
--- src/main/resources/templates/pizzashop.ftl
+++ src/main/resources/templates/pizzashop.ftl
@@ -4,6 +4,12 @@
         <head>
             <meta charset=utf-8>
             <title>Pizzashop HTML</title>
+            <style>
+                table {
+                    border-collapse: collapse;
+                    width: 300px;
+                }
+            </style>
 
         </head>
         <body background=http://cdn4.micasa.ch/medias/sys_master/zoom/1/c/5/6/id_10295631904798_zoom.jpg><b>
@@ -12,14 +18,8 @@
             </a><br>
             <br>
             Pizza lieferung:
-            <a href=https://www.dieci.ch/de/>bitte hier klicken um Die bestellung zu machen.<br></a><br>
+            <a href=https://www.dieci.ch/de/ target="_blank">bitte hier klicken um Die bestellung zu machen.<br></a><br>
             Zur auswahl stehen folgende Sorten mit dem Preis f&uumlr die Kleine Pizza
-            <br>
-            <br>
-
-            <form action="PizzaServler" method="post">
-                <input type="submit" value="button">
-            </form>
 
             <br>
             <br>
@@ -28,11 +28,14 @@
                 <table>
                     <#list pizzas as pizza>
                         <tr>
-                            <th>${pizza.name}</th>
+                            <td>${pizza.name}</td>
                             <td>${pizza.price}</td>
+                            <td>id: ${pizza.id}</td>
                         </tr>
                     </#list>
                 </table>
+            <#else>
+                Es sind keine Pizzas gespeichert
             </#if>
 
 

f7259112c08fc872d734b5488e753aeb1d847c2a
Daniel Löffeler <daniel.loeffler@ti8m.ch>
2019-01-30 16:24:32 +0100

    DAO Interfaces kommentiert

--- src/main/java/ch/ti8m/azubi/lod/pizzashop/persistence/PizzaDAO.java
+++ src/main/java/ch/ti8m/azubi/lod/pizzashop/persistence/PizzaDAO.java
@@ -7,34 +7,29 @@ import java.util.List;
 public interface PizzaDAO {
 
     /**
-     *
      * @return Returns a list all Pizzas
      * @throws Exception a exception for the list
      */
     List<Pizza> getPizzas() throws Exception;
 
     /**
-     *
      * @param id ID of a Pizza
      * @return Pizza of the requested Pizza
      */
     Pizza getPizzaByID(int id);
 
     /**
-     *
      * @param pizza creates Pizza
      */
     void create(Pizza pizza);
 
     /**
-     *
      * @param pizza Pizza to change
-     * @param id ID of the Pizza to change
+     * @param id    ID of the Pizza to change
      */
     void update(Pizza pizza, int id);
 
     /**
-     *
      * @param id ID of Pizza to delete
      */
     void delete(int id);
